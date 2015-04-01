7265a58e799c8c872a4bbbd4920d98d22e2e8bc3
Geert <Geert@145.107.79.72>
2015-04-01 14:42:28 +0200

    lfdlkjfad

---
+++ mmmiinfo_2/src/com/gl/mmmiinfo_2/PlayerData.java
@@ -0,0 +1,20 @@
+package com.gl.mmmiinfo_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerData extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.playerdata);
+		
+	}
+	
+	
+}
